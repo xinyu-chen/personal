afff34a2f0337119a53e648f7230c722834d1a69
XY <xinyu.chen@behalf.com>
2018-01-02 14:42:23 -0500

    Changed google service account

--- app/paymentReport/__init__.py
+++ app/paymentReport/__init__.py
@@ -6,7 +6,7 @@ direct = sys.argv[0].split('/')
 direct = '/'.join(direct[:-1]) + "/paymentReport"
 
 # Create credential as an environmental variable
-json_key = direct + '/SF-backup-411eeb87f9ca.json'
+json_key = direct + '/XY private-26235b64179e.json'
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_key
 
 
--- app/paymentReport/peeriq_upload.py
+++ app/paymentReport/peeriq_upload.py
@@ -8,73 +8,58 @@ from oauth2client.client import GoogleCredentials
 download_folder = '/opt/behalf/cto/reporting/data'
 
 def get_sql(date):
-    query = """SELECT *, CASE WHEN LoanStatus='Charged Off' THEN PrincipalOutstanding Else 0 END as NetLoss,CASE WHEN LoanStatus='Charged Off' THEN ROUND(DefaultAmount-PrincipalOutstanding,2) ELSE 0 END as Recoveries   
-        FROM (   
-        SELECT ID as LoanID, 'Not Funded' as FundingStatus, CASE WHEN Is_Finwise IS FALSE THEN 'MCA' ELSE 'Loan' END as TrxType,  
-        CASE WHEN Is_Finwise IS TRUE then 1 ELSE 0 END as PctFW, Payout_Method as PayoutMethod, AccountID as CustomerID, CASE WHEN trxs.CustomerFICO>900 THEN null ELSE trxs.CustomerFICO END as CustomerFICO,  
-        CreditLimit, trxs.CustomerState as CustomerState, 0 AS BusinessAge, 
-        CASE WHEN acct.CLOED is not null AND acct.Credit_Limit >100000 THEN Date_ADD(acct.CLOED,-6,'MONTH')  
-             WHEN acct.CLOED is not null AND acct.Credit_Limit BETWEEN 50000 AND 100000 THEN Date_ADD(acct.CLOED,-12,'MONTH') ELSE null END as LastReUW,  
-        Supplier_Name as SupplierName,Supplier_Industry as SupplierIndustry,   
-        CASE WHEN Payout_Date is not null THEN DATE(Payout_Date) ELSE DATE(Payout_Hit_Date_New) END as PayoutDate, Date(First_repayment_date) as FirstRepaymentDate, Date(pmts.Next_Debit_date) As NextRepaymentDate,   
-        Date(Last_AR_Date) as LastRepaymentDate, DATE(ORGLRDate) as OrigLastRepaymentDate,  
-        Num_Days as Term, Repayment_Frequency as Product,  NARep as PlannedRepayments, Num_Repayments as OrigPlannedRepayments, nsRep as SettledRepayments, round(Total_Amount_To_Collect_Without_Fees/Num_Repayments,2) as RepaymentAmount,   
-        ROUND(100*(Total_Customer_Fees)/(Amount*Num_Days/30),2) as TrxMPR,   
-        Amount,Total_Paid_Out_To_Supplier as PayoutAmount,   
-        Total_Customer_Fees as CustomerInterest,FinFee+Collection_Fees+CC_Fees as CustomerFees,Total_Supplier_Fees as SupplierFees,   
-        CASE WHEN ROUND((Total_Received-Total_Paid_Out_To_Customer)*Amount/(Amount+Total_Customer_Fees),2)>Amount THEN Amount   
-             ELSE ROUND((Gross_Total_Received_From_Customer-Total_Paid_Out_To_Customer)*Amount/(Amount+Total_Customer_Fees),2)   
-                  + (CASE WHEN Amount>=Total_Paid_Out_To_Supplier THEN Total_Received_From_Supplier ELSE 0 END) END as PrincipalPaidToDate,   
-        CASE WHEN ROUND((Total_Received-Total_Paid_Out_To_Customer)*(1-Amount/(Amount+Total_Customer_Fees)),2)>Total_Customer_Fees THEN Total_Customer_Fees   
-             ELSE ROUND((Gross_Total_Received_From_Customer-Total_Paid_Out_To_Customer)*(1-Amount/(Amount+Total_Customer_Fees)),2) END    
-             as InterestPaidToDate,   
-        CASE WHEN ROUND((Total_Received-Total_Paid_Out_To_Customer)*(1-Amount/(Amount+Total_Customer_Fees)),2)<0   
-             THEN Amount+Total_Customer_Fees+FinFee+Collection_Fees+CC_Fees-Total_Received_From_Customer-Total_Paid_Out_To_Customer   
-             ELSE 0 END as CustomerFeesPaidToDate,   
-        CASE WHEN Total_Received_From_Supplier+IFNULL(Credit_Note,0)>=Total_Paid_Out_To_Supplier THEN 0   
-             WHEN Amount-(Total_Received-Total_Paid_Out_To_Customer)*Amount/(Amount+Total_Customer_Fees)<1 AND Amount>=Total_Paid_Out_To_Supplier THEN 0   
-             ELSE ROUND(Amount-(CASE WHEN Amount>=Total_Paid_Out_To_Supplier THEN Total_Received_From_Supplier ELSE 0 END)   
-                              -(Total_Received_From_Customer-Total_Paid_Out_To_Customer+Vendor_Refund)*(Amount/(Amount+Total_Customer_Fees)),2) END as PrincipalOutstanding,   
-        CASE WHEN Total_Received_From_Supplier+IFNULL(Credit_Note,0)>=Total_Paid_Out_To_Supplier THEN 0   
-             WHEN ROUND(Total_Customer_Fees-(Gross_Total_Received_From_Customer-Total_Paid_Out_To_Customer)*(1-Amount/(Amount+Total_Customer_Fees)),2)<0 THEN 0   
-             ELSE ROUND(Total_Customer_Fees-(Gross_Total_Received_From_Customer-Total_Paid_Out_To_Customer)*(1-Amount/(Amount+Total_Customer_Fees)),2) END as InterestOutstanding,   
-        CASE WHEN FinFee+Collection_Fees+CC_Fees=0 THEN 0   
-             WHEN Total_Received_From_Supplier+IFNULL(Credit_Note,0)>=Total_Paid_Out_To_Supplier THEN 0   
-             WHEN ROUND(Total_Customer_Fees-(Gross_Total_Received_From_Customer-Total_Paid_Out_To_Customer)*(1-Amount/(Amount+Total_Customer_Fees)),2)<0 THEN   
-                  ROUND(Amount+FinFee+Collection_Fees+CC_Fees+Total_Customer_Fees-Total_Received-Total_Paid_Out_To_Customer,2)   
-             ELSE ROUND(FinFee+Collection_Fees+CC_Fees,2) END as CustomerFeesOutstanding,   
-        CASE WHEN Total_Received_From_Supplier+IFNULL(Credit_Note,0)>=Total_Paid_Out_To_Supplier THEN 'Principal Paid Up'   
-             WHEN Amount-(Total_Received-Total_Paid_Out_To_Customer)*Amount/(Amount+Total_Customer_Fees)<1 AND Late_Bucket_181d_turn_over is null THEN 'Principal Paid Up'   
-             WHEN Late_Bucket_181D_Turn_Over IS NULL AND Late_Bucket='Principal Paid Up' AND (Aging_Current_Future>0 OR date(Last_Ar_Date)>=current_date()) THEN 'Current'   
-             WHEN Late_Bucket_181D_Turn_Over IS NULL AND Late_Bucket='Principal Paid Up' AND (Aging_more_120_Days>0 OR ROUND(Amount-(Total_Received-Total_Paid_Out_To_Customer)*(Amount/(Amount+Total_Customer_Fees)),2)>1   
-             ) AND Aging_Current_Future=0 THEN 'Charged Off'   
-             WHEN Late_Bucket_181D_Turn_Over IS NULL THEN Late_Bucket WHEN Late_Bucket IS NULL THEN 'Current'   
-             ELSE 'Charged Off' END as LoanStatus,   
-        Late_Bucket_Num_Days as DaysOverdue,   
-        CASE WHEN Total_Received_From_Supplier+IFNULL(Credit_Note,0)>=Total_Paid_Out_To_Supplier THEN 0   
-             WHEN Late_Bucket_181D_Turn_Over IS NULL AND Late_Bucket='Principal Paid Up' AND (Aging_Current_Future>0 OR date(Last_Ar_Date)>=current_date()) THEN 0   
-             WHEN (Late_Bucket_181D_Turn_Over IS NOT NULL) AND (Amount-(Total_Received-Total_Paid_Out_To_Customer)*Amount/(Amount+Total_Customer_Fees)) > Late_bucket_181D_Turn_Over_Amount   
-             THEN ROUND(Amount-Total_Received_From_Supplier-(Gross_Total_Received_From_Customer-Total_Paid_Out_To_Customer)*Amount/(Amount+Total_Customer_Fees),2)   
-             WHEN (Late_Bucket_181D_Turn_Over IS NOT NULL) AND (Amount-(Total_Received-Total_Paid_Out_To_Customer)*Amount/(Amount+Total_Customer_Fees)) <= Late_bucket_181D_Turn_Over_Amount   
-             THEN ROUND(Amount-(Total_Paid_Out_To_Supplier-Late_Bucket_181D_Turn_Over_Amount)*Amount/(Amount+Total_Customer_Fees),2)   
-             WHEN Late_Bucket='Principal Paid Up' AND ROUND(Amount-(Total_Received-Total_Paid_Out_To_Customer)*Amount/(Amount+Total_Customer_Fees),2)>1   
-             THEN ROUND(Amount-Total_Received_From_Supplier-(Gross_Total_Received_From_Customer-Total_Paid_Out_To_Customer)*Amount/(Amount+Total_Customer_Fees),2)   
-             ELSE 0 END as DefaultAmount,Late_Bucket_181D_Turn_Over as ChargeOffDate   
-             FROM [sf-backup-973:"""+date+""".Opportunity] as opp   
-        JOIN [bi-tables:CustomizedTables.TrxsRawExtended] as trxs   
-        ON trxs.TrxId=opp.ID   
-        JOIN (SELECT ID as AcctID, CLOED, Credit_Limit FROM [sf-backup-973:"""+date+""".Account] ) as acct 
-        ON trxs.CustomerID=acct.AcctID 
-        LEFT JOIN (
-        SELECT Trx, MIN(Hit_Date) as Next_Debit_Date FROM [sf-backup-973:"""+date+""".Payment] 
-        WHERE RecordTypeID='012b00000000HbwAAE' 
-        AND (Status='Halted Manually' OR Status='Halted Automatically' OR Status='Waiting for Scheduling' OR Status='In Process')
-        GROUP BY 1
-        ) as pmts
-        ON trxs.TrxID=pmts.Trx
-        WHERE Valid_For_Stats_extended is true AND Live_Status<>'Cancelled By Zazma' AND Live_Status<>'Cancelled By Supplier' AND Live_Status<>'Cancelled By Customer'   
-        ) WHERE PayoutDate is not null AND PayoutAmount>0  
-        ORDER BY PayoutDate DESC"""
+    query = """SELECT *, CASE WHEN LoanStatus='Charged Off' THEN PrincipalOutstanding Else 0 END as NetLoss,
+    CASE WHEN LoanStatus='Charged Off' THEN GREATEST(ROUND(DefaultAmount-PrincipalOutstanding,2),0) ELSE 0 END as Recoveries   
+    FROM (   
+         SELECT ID as LoanID, CASE WHEN Is_Trx_Eligible_for_Debt_Facility is true THEN 'Eligible' ELSE 'Ineligible' END as EligibilityStatus,
+         Debt_Facility_Funding_Status as FundingStatus, CASE WHEN Is_Finwise IS FALSE THEN 'MCA' ELSE 'Loan' END as TrxType,  
+         CASE WHEN Is_Finwise IS TRUE then 1 ELSE 0 END as PctFW, Payout_Method as PayoutMethod, AccountID as CustomerID, 
+         CASE WHEN trxs.CustomerFICO>900 THEN null ELSE trxs.CustomerFICO END as CustomerFICO,  
+         CreditLimit, trxs.CustomerState as CustomerState, 0 AS BusinessAge, 
+         CASE WHEN (CASE WHEN Payout_Date is not null THEN DATE(Payout_Date) ELSE DATE(Payout_Hit_Date_New) END)>=DATE(uw.FirstManualUWDate) THEN true ELSE false END as ManualUW, 
+         CASE WHEN acct.CLOED is not null AND acct.Credit_Limit >100000 THEN Date_ADD(acct.CLOED,-6,'MONTH')  
+              WHEN acct.CLOED is not null AND acct.Credit_Limit BETWEEN 50000 AND 100000 THEN Date_ADD(acct.CLOED,-12,'MONTH') ELSE null END as LastReUW, 
+         uw.Annual_Revenue as AnnualRev, Supplier_Name as SupplierName,Supplier_Industry as SupplierIndustry,   
+         CASE WHEN Payout_Date is not null THEN DATE(Payout_Date) ELSE DATE(Payout_Hit_Date_New) END as PayoutDate, 
+         Date(First_repayment_date) as FirstRepaymentDate, Date(pmts.Next_Debit_date) As NextRepaymentDate,   
+         Date(Last_AR_Date) as LastRepaymentDate, Is_Restructured as Restructured, DATE(ORGLRDate) as OrigLastRepaymentDate,  
+         Num_Days as Term, Repayment_Frequency as Product, Num_Repayments as OrigPlannedRepayments, NARep as PlannedRepayments, nsRep as SettledRepayments, 
+         ROUND((Amount+Total_Customer_Fees+FinFee)/(CASE WHEN NARep=0 THEN Num_Repayments ELSE NARep END),2) as RepaymentAmount,   
+         ROUND(100*(Total_Customer_Fees+FinFee)/(Amount*Num_Days/30),2) as TrxMPR,   
+         Amount,Total_Paid_Out_To_Supplier as PayoutAmount, Total_Customer_Fees+FinFee as CustomerInterest,ROUND(Collection_Fees+CC_Fees,2) as CustomerFees,
+         Total_Supplier_Fees as SupplierFees, ROUND(POPA,2) as PrincipalPaidToDate, ROUND(POIA,2) as InterestPaidToDate,   
+         CASE WHEN Collection_Fees+CC_Fees>0 
+              THEN ROUND(Gross_Total_Received_From_Customer+Total_Received_From_Supplier+Total_Repayments_In_Process-POPA-POIA,2)
+              ELSE 0 END as CustomerFeesPaidToDate,
+         ROUND(NPOPA,2) as PrincipalOutstanding, ROUND(Total_Customer_Fees+FinFee-POIA,2) as InterestOutstanding,
+         CASE WHEN Collection_Fees+CC_Fees>0 
+              THEN ROUND(Collection_Fees+CC_Fees-(Gross_Total_Received_From_Customer+Total_Received_From_Supplier+Total_Repayments_In_Process-POPA-POIA),2)
+              ELSE 0 END as CustomerFeesOutstanding, 
+         CASE WHEN Late_Bucket_181D_Turn_Over is not null THEN 'Charged Off'
+              WHEN NPOPA<=0 THEN 'Principal Paid Up'   
+              WHEN Late_Bucket IS NULL THEN 'Current' 
+              WHEN Late_Bucket_181D_Turn_Over IS NULL THEN Late_Bucket   
+              ELSE 'Charged Off' END as LoanStatus,   
+         Late_Bucket_Num_Days as DaysOverdue, Late_Bucket_181D_Turn_over_Amount as DefaultAmount,Late_Bucket_181D_Turn_Over as ChargeOffDate   
+         FROM [bi-tables:Salesforce.Opportunity] as opp   
+         JOIN [bi-tables:CustomizedTables.TrxsRawExtended] as trxs  
+         ON trxs.TrxId=opp.ID   
+         JOIN (SELECT ID as AcctID, CLOED, Credit_Limit FROM [bi-tables:Salesforce.Account]) as acct 
+         ON trxs.CustomerID=acct.AcctID 
+         LEFT JOIN (  
+              SELECT Account,MIN(CreatedDate) as FirstManualUWDate, LAST(Annual_Revenue) as Annual_Revenue   
+              FROM [zazma.com:beaming-storm-829:salesforce.Risk_Financials]   
+              WHERE Risk_Decision='Approved' GROUP BY 1  
+         ) as uw ON opp.AccountID=uw.Account  
+         LEFT JOIN ( 
+              SELECT Trx, MIN(Hit_Date) as Next_Debit_Date 
+              FROM [bi-tables:Salesforce.Payment] 
+              WHERE RecordTypeID='012b00000000HbwAAE' AND (Status='Waiting for Scheduling' OR Status='In Process') GROUP BY 1 
+         ) as pmts 
+         ON trxs.TrxID=pmts.Trx 
+         WHERE Valid_For_Stats_extended is true AND Live_Status<>'Cancelled By Zazma' AND Live_Status<>'Cancelled By Supplier' AND Live_Status<>'Cancelled By Customer'
+    ) WHERE PayoutDate is not null AND PayoutAmount>0
+    ORDER BY PayoutDate DESC, LoanID DESC"""
     return query
 
 def create_table(date, dataset_id, table_id):
