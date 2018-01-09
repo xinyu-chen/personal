0b0b859c1b29723749b43b03307e657607e09b8a
Chaim - Behalf <chaim@behalf.com>
2018-01-09 13:29:35 +0200

    fix file name and location

--- app/paymentReport/__init__.py
+++ app/paymentReport/__init__.py
@@ -5,8 +5,10 @@ import sys
 direct = sys.argv[0].split('/')
 direct = '/'.join(direct[:-1]) + "/paymentReport"
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 # Create credential as an environmental variable
-json_key = direct + '/XY private-26235b64179e.json'
+json_key = dir_path + '/XY-private-26235b64179e.json'
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_key
 
 
